fc3b00ddc17478aa1899692779e598e8afcfce28
jorge pinedo <jpinedom@hotmail.com>
2018-05-06 17:46:18 -0500

    add keys db

--- class/Conexion.php
+++ class/Conexion.php
@@ -17,16 +17,17 @@ class Conexion {
     public function Conectar() {
 //        $con = pg_connect("host=localhost port=5432 dbname=empleado user=postgres password=123");
 
-        $con = pg_connect("host=empleado.cddjetfe34nc.us-east-2.rds.amazonaws.com port=5432 dbname=empleado user=empleado password=empleado2018") 
-                or die("No se ha podido conectar ". pg_last_error()." error"); 
+//        $con = pg_connect("host=empleado.cddjetfe34nc.us-east-2.rds.amazonaws.com port=5432 dbname=empleado user=empleado password=empleado2018") 
+        return pg_connect("host=empleado.cddjetfe34nc.us-east-2.rds.amazonaws.com port=5432 dbname=empleado user=empleado password=empleado2018");
+                
 
-        $stat = pg_connection_status($con);
-        if ($stat === PGSQL_CONNECTION_OK) {
-            return $con;
-        } else {
-            echo "Error, Problemas al conectar con el servidor";
-            exit;
-        }
+//        $stat = pg_connection_status($con);
+//        if ($stat === PGSQL_CONNECTION_OK) {
+//            return $con;
+//        } else {
+//            echo "Error, Problemas al conectar con el servidor";
+//            exit;
+//        }
         
     }
 
